33dfefb68dcbe50b1f2dfe1856935979762fcbc3
Cristian Alderete <cristianalderete911@gmail.com>
2025-02-15 12:55:43 -0300

    prueba3

--- js/carrito.js
+++ js/carrito.js
@@ -50,7 +50,7 @@ function mostrarProductos(productos, containerId) {
         const productoDiv = document.createElement('div');
         productoDiv.classList.add('producto');
         //const imageVistas = window.location.pathname.includes('menu.html') ? `./${producto.imagen}` : `../${producto.imagen}`;// operador ternario(error al cargar las imagenes, forma resumida)
-        const imagenCarga = `${basePath}${producto.imagen}`;
+        const imagenCarga = producto.imagen;
         productoDiv.innerHTML = `
             <div class="icono-compra" data-id="${producto.id}">
                 <i class="fas fa-shopping-cart"></i>
